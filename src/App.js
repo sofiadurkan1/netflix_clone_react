38d749932a3cf2ca4bcfa20b61745c50a39dc568
sofia durkan <sofiadurkan@gmail.com>
2021-09-01 10:22:33 -0500

    Jumbotron added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import React from 'react';
-import jumboData from './fixtures/jumbo';
-import Jumbotron from "./components/jumbotron"
-
+import Jumbotron from "./components/jumbotron";
+import jumboData from "./fixtures/jumbo";
 
 function App() {
   return (
-    <Jumbotron.Container>
-      {jumboData.map((item) => {
-        <Jumbotron key ={item.id} direction={item.direction}>
-          <p>hello</p>
+    <div className="App">
+      <Jumbotron.Container>
+        {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image src={item.image} alt={item.alt} />
+            </Jumbotron.Pane>
           </Jumbotron>
-      })}
-
-
-
-    </Jumbotron.Container>
-
-
-  
+        ))}
+      </Jumbotron.Container>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import {render} from "react-dom";
+import 'normalize.css';
 import App from "./app";
+import { GlobalStyles}  from "./global-styles";
 
-render(<App />, document.getElementById("root"));
+render(<>
+<GlobalStyles/>
+<App />
+</>,
+     document.getElementById("root"));
