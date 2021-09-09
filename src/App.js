a48a1c5826aa96036dd02eeba3c28ff6eefdbde9
Sofia <sofiadurkan@gmail.com>
2021-09-09 10:28:38 -0500

    netflix-clone completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Row from "./Row";
 import requests from './requests';
 import Banner from "./Banner";
+import Nav from './Nav';
 
 
 function App() {
   return (
     <div className="app">
+      <Nav/>
       
       <Banner/>
       {/* <Row/> */}
